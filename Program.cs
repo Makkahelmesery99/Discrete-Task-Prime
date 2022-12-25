66e073d8137e6c6adc753dec9828805be48e0cab
Makkahelmesery99 <121176245+Makkahelmesery99@users.noreply.github.com>
2022-12-25 21:04:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the last number");
+            int last = int.Parse(Console.ReadLine());
+            for (int i = first; i <= last; ++i)
+            {
+                int flag = 1;
+                for (int j = 2; j < i; ++j)
+                {
+                    if (i % j == 0)
+                        flag = 0;
+                    if (flag == 1)
+                        Console.WriteLine(i);
+                }
+
+
+            }
+        }
+    }
+}
